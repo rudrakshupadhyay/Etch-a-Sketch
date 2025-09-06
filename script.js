ab4999452e5bb2bbb2d0f0e23293b9e960650464
Rudraksh Upadhyay <218912408+rudrakshupadhyay@users.noreply.github.com>
2025-09-06 22:55:33 +0530

    create dynamic box in continer

---
+++ script.js
@@ -0,0 +1,36 @@
+function createbox(number_square){
+    workspace.innerHTML = ""; // clear old boxes before creating new ones
+    let boxSize = 500/number_square;
+    for(let i = 1;i <= number_square*number_square; i++){
+        let box = document.createElement("div");
+        box.style.width = boxSize + "px";
+        box.style.height = boxSize + "px";
+        box.style.border = "1px solid gray";
+        box.style.boxSizing = "border-box";
+        workspace.appendChild(box);
+    }
+}
+function input(){
+    let number_square = prompt("Enter n in n*n grid maximum value 100");
+    if (number_square === null || number_square.trim() === "" || number_square > 100) {
+        number_square = Math.floor(Math.random()*100);
+    }
+    return number_square;
+}
+
+let number_square = input();
+let heading = document.querySelector(".top");
+let content = document.createElement("div");
+content.textContent  = `Squares per side: ${number_square}`;
+heading.appendChild(content);
+let workspace = document.querySelector(".workspace");
+createbox(number_square);
+
+let reset = document.querySelector(".setSquare");
+reset.addEventListener("click",() => {
+    let newNumber = input();
+    content.textContent  = `Squares per side: ${newNumber}`;
+    createbox(newNumber);
+})
+
+
